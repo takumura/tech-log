e90be1817ce173f3a6cc9e09ea105278afa28cc2
takumura <takumura@outlook.com>
2020-02-10 01:17:12 +0900

    fix: modify route transition issue

--- Website/ClientApp/src/app/shared/route-transformer.directive.ts
+++ Website/ClientApp/src/app/shared/route-transformer.directive.ts
@@ -7,22 +7,19 @@ import { LoadingBarService } from 'src/app/core/loading-bar/loading-bar.service'
   selector: '[appRouteTransformer]',
 })
 export class RouteTransformerDirective {
-  constructor(
-    private router: Router,
-    @Inject('BASE_URL') private baseUrl: string,
-    private loadingBarService: LoadingBarService,
-  ) {}
+  constructor(private router: Router, @Inject('BASE_URL') private baseUrl: string, private loadingBarService: LoadingBarService) {}
 
   @HostListener('click', ['$event'])
   public onClick(event) {
     if (event.target.tagName === 'A') {
       if (event.target.classList && event.target.classList[0] === 'internal-link') {
+        const targetPath = event.target.href.replace(this.baseUrl, '/').replace(/#.*/, '');
         if (event.target.hash) {
-          this.router.navigate([event.target.pathname], {
+          this.router.navigate([targetPath], {
             fragment: event.target.hash.slice(1),
           });
         } else {
-          this.router.navigate([event.target.pathname]);
+          this.router.navigate([targetPath]);
         }
         event.preventDefault();
       } else if (event.target.classList && event.target.classList[0] === 'internal-anchor') {
