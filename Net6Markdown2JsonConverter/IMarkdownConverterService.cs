5891e98ce6e72da8a14b65193f5a2701769187a5
takumura <takumura@outlook.com>
2022-12-31 11:24:41 +0900

    refactor: use DocuPacker library for backent

--- Net6Markdown2JsonConverter/IMarkdownConverterService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Net6MarkdownWebEngine.Converter;
-
-public interface IMarkdownConverterService
-{
-    ValueTask RunAsync(CommandLineOptions options);
-    ValueTask ConvertAsync(string? input, string? output, string? indexDir, DateTime? dateFrom = null);
-}
-
--- Net6Markdown2JsonConverter/Utils/FileComparisonModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Net6MarkdownWebEngine.Converter;
-
-public class FileComparisonModel
-{
-    public string FullPath { get; set; } = string.Empty;
-    public string DirectoryFrom { get; set; } = string.Empty;
-    public string RelativePath { get; set; } = string.Empty;
-    public string FileName { get; set; } = string.Empty;
-}
