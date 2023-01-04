7413b5053adc7b9d8e096451879bd9e439e827fc
takumura <takumura@outlook.com>
2023-01-04 17:18:36 +0900

    chore: revive WebApi imple for test

--- TechLogAngularStandalone/src/main.ts
+++ TechLogAngularStandalone/src/main.ts
@@ -1,22 +1,19 @@
-import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom, isDevMode } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes, TitleStrategy } from '@angular/router';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
 import { AppComponent } from './app/app.component';
 import { MyTitleStrategyService } from './app/shared/services/markdown-document-title-strategy.service';
 import { reducers } from './app/store';
 import { DocumentIndexStoreModule } from './app/store/document-index/document-index-store.module';
 import { DocumentSearchStoreModule } from './app/store/document-search/document-search-store.module';
 import { LoadingStoreModule } from './app/store/loading/loading-store.module';
-
-export function getBaseUrl() {
-  return document.getElementsByTagName('base')[0].href;
-}
 
 const routes: Routes = [
   {
@@ -48,7 +45,6 @@ const routes: Routes = [
 
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] },
     { provide: TitleStrategy, useClass: MyTitleStrategyService },
     importProvidersFrom(
       BrowserAnimationsModule,
